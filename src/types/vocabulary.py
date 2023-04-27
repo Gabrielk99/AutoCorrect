8985063c239072a173d0eeffc57a82c91d378987
Gabrielk99 <gabriel_100nocao@hotmail.com>
2023-04-26 21:40:57 -0300

    feature/vocabulario construído por corpus ou vocabulario

--- src/types/vocabulary.py
+++ src/types/vocabulary.py
@@ -4,15 +4,16 @@ from dataclasses import dataclass
 from unidecode import unidecode
 from src.types.singleton import SignletonMetaclass
 
-@dataclass(frozen=True)
+@dataclass()
 class Vocabulary(metaclass=SignletonMetaclass):
-  _counter_vocabulary:Counter
-  
-  def __init__(self,corpus:str=None) -> None:
-    if corpus is not None:
+  _counter_vocabulary:Counter = None
+  _vocabulary:list = None
+  def __init__(self,corpus:str=None,collections:list=None) -> None:
+    if corpus is not None and self._counter_vocabulary is None:
       corpus = corpus.lower()
       corpus = unidecode(corpus)
       tokens = re.findall(r'\w+',corpus)
       self._counter_vocabulary = Counter(tokens)
-    
+    if collections is not None and self._vocabulary is None:
+      self._vocabulary = collections
   
